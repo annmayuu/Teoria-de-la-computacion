af04b9105f3a8eaab011d97827a5f5bfa1b53123
May <annmayuu@gmail.com>
2024-07-08 20:22:52 -0600

    Practicas

---
+++ Practica 2/entrada.java
@@ -0,0 +1,19 @@
+public class EjemploPracticaAnalizador {
+    public static void main ( String[] args ) {
+        /* int octal1 = -01813 + 123.123 + 0XFFFF , octal2 = 0371 ;
+        short dato = 0xA12 ;
+        */
+        double PI = 3.1416 , CteGrav = -6.674E-19 ;
+        float prom = (float)1.2 ;
+        double val = 1.2 == 2 * ;
+        /* Calculos generales 
+        espero q esta cosa este bien
+        supongo q si */
+        for ( int i = 0 ; i < 10 ; i++ ) {
+            prom += i * 35 ;
+        }
+        double pot = 7.34E+1.2 ;
+        public = CteGrav * dato * pot ;
+        System.out.println ( " Prom= " + ( pr#om / PI ) + " Result= " + ( var * validID ) ) ;
+    }
+}
